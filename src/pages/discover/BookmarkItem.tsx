44503b094dc3adcd750b8e8ce94c8b69b2b821ff
sim <sim@terra.money>
2022-05-10 16:51:16 +0900

    wip: desc

--- src/pages/discover/BookmarkItem.tsx
+++ src/pages/discover/BookmarkItem.tsx
@@ -2,17 +2,21 @@ import { ExternalLink } from "components/general"
 import styles from "./BookmarkItem.module.scss"
 
 export interface BookmarkProps {
-  name: string
   url: string
+  name: string
+  description: string
+  image: string
 }
 
-const BookmarkItem = ({ name, url }: BookmarkProps) => {
+const BookmarkItem = ({ url, name, description, image }: BookmarkProps) => {
   const { hostname } = new URL(url)
 
   return (
     <ExternalLink href={url} className={styles.component}>
+      <img src={image} alt="" width={50} height={50} />
       <h1 className={styles.title}>{name}</h1>
       <p className="small muted">{hostname}</p>
+      <p className={styles.description}>{description}</p>
     </ExternalLink>
   )
 }
